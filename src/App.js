687cf6e6dce4626ae702530b76f28d476f3ac5b8
Arthur BAUZOU <arthurbauzou@gmail.com>
2023-12-14 11:22:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+// import './components/skill'
+
+function Skill() {
+  return (
+      <div className="skill">
+          <h2>COMPETENCE</h2>
+          <p>haha c’est la compétence lol</p>
+      </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Skill />
       </header>
     </div>
   );
---
+++ src/components/skill.js
@@ -0,0 +1,8 @@
+function Skill() {
+    return (
+        <>
+            <h2>COMPETENCE</h2>
+            <p>haha c’est la compétence lol</p>
+        </>
+    )
+}
